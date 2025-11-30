4b3e25b6bd12fb699afa0fbf294fd6b5cc8dbb95
Midas <midasscheffers@gmail.com>
2025-11-30 22:45:24 +0100

    [add] human readable squares in gen moves

--- main/midmac.c
+++ main/midmac.c
@@ -19,15 +19,19 @@ int main() {
     int _ = LoadFen(START_FEN, board);
     PrintBoard(board);
     printf("\n");
-    // BoardPrintBitBorads(board);
 
+    // move gen
     generateMoves(board);
     printf("Generated moves:\n");
     for (int i = 0; i<MAX_POS_MOVES_ONE_POS;++i){
         if (possible_moves[i] == NULL_MOVE) break;
         unsigned int  start = possible_moves[i]&0b111111;
         unsigned int  target = (possible_moves[i]&0b111111000000)>>6;
-        printf("Move: %d, %d\n", start, target);
+        char s_r = 8-start/8 + '0';
+        char s_f = start%8 + 'a';
+        char t_r = 8-target/8 + '0';
+        char t_f = target%8 + 'a';
+        printf("Move: %c%c, %c%c\n", s_f, s_r, t_f, t_r);
     }
 
     printf("\n");
